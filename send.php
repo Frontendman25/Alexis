48a6cf5db440dbd0b8bb92daa28156a874ac7b4f
Frontendman25 <designer24@i.ua>
2018-07-02 15:32:18 +0300

    adding4

--- send.php
+++ send.php
@@ -1,7 +1,6 @@
 <?php
 //Принимаем постовые данные
 $name=$_POST['name'];
-$phone=$_POST['phone'];
 $email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
@@ -13,11 +12,11 @@ $from_site = "Заявка с сайта";
 // Сообщение письма
 $message = "
 Имя пользователя: ".htmlspecialchars($name)."<br />
-Телефон: <a href='tel:$phone'>".htmlspecialchars($phone)."</a>";
+Телефон: <a href='tel:$email'>".htmlspecialchars($email)."</a>";
 // Отправляем письмо при помощи функции mail();
-$headers = "From: stastroi.ru <mail@stastroi.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+$headers = "From: alexis <designer24@i.ua>\r\nContent-type: text/html; charset=UTF-8 \r\n";
 mail ($to, $from_site, $message, $headers);
 // Перенаправляем человека на страницу благодарности и завершаем скрипт
-header('Location: thanks.html');
+//header('Location: thanks.html');
 exit();
 ?>
